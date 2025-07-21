446dba5218035cde0a4de6541e448a0760773abf
Malik Talha <realmaliktalha@gmail.com>
2025-07-21 20:31:08 +0500

    add constraints and exceptions for uploading resume (PDF file)

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 
-from fastapi import FastAPI, UploadFile, File
-import uvicorn
+from fastapi import FastAPI, UploadFile, status, HTTPException
 
 from langchain_core.runnables import Runnable
 
 from data_models.models import ResumeData, JobDescription, Tone
 from services.pdf_service import extract_pdf
 from services.ai.ai_service import get_resume_extraction_chain, get_cover_writer_chain
+
+import uvicorn
 
 # from dotenv import load_dotenv
 # load_dotenv()
@@ -37,7 +38,22 @@ def root():
 @app.post("/cover-letter/resume")
 # async def analyze_resume(job_description: JobDescription, tone: Tone, resume: UploadFile):
 async def analyze_resume_and_write_cover_letter(job_title: str, job_description: str, tone: Tone, resume: UploadFile):
+    """ write a cover letter by analyzing the uploaded resume (PDF Only!) and by observing the job description """
+
+    if resume.content_type != "application/pdf": 
+        logger.critical(f"The uploaded file \"{resume.filename}\" is not a PDF!")
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail=f"Unsupported file type. Expected PDF, but got {resume.content_type}."
+        )
     
+    elif (resume.size/1000000) > 5: # if pdf is more than 5 MB
+        logger.critical(f"The size of the uploaded file ({resume.size/1000000:.2f} MB) is exceeding the 5 MB limit!")
+        raise HTTPException(
+            status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+            detail=f"The size of the PDF (resume) you uploaded is {resume.size/1000000:.2f} MB which is exceeding the 5 MB limit!"
+        )
+
     # extract resume text
     text = await extract_pdf(resume)
 
